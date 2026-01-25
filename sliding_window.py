951c49c418f987c24bbc2bcc90ce0e337ded5c3b
Anton <ivanko.anton1408@gmail.com>
2026-01-25 17:34:38 -0500

    Add example usage commen
    # Conflicts:
    #	sliding_window.py

--- sliding_window.py
+++ sliding_window.py
@@ -6,14 +6,22 @@ class Solution:
         left = 0
         
         for right in range(n):
-            if charIndex[ord(s[right])] >= left:
-                left = charIndex[ord(s[right])] + 1
-            charIndex[ord(s[right])] = right
+            left = self.new_method(s, charIndex, left, right)
             maxLength = max(maxLength, right - left + 1)
         
         return maxLength
+<<<<<<< HEAD
         
         
 # Example usage:
+=======
+
+    def new_method(self, s, charIndex, left, right):
+        if charIndex[ord(s[right])] >= left:
+            left = charIndex[ord(s[right])] + 1
+        charIndex[ord(s[right])] = right
+        return left
+# Example  usage:
+>>>>>>> b674656 (Add example usage commen)
 solution = Solution()
 print(solution.lengthOfLongestSubstring("abcabcbb"))  # Output: 3
