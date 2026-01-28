40457adebf13edb95539c0ba4924b17f8024f63b
Anton <ivanko.anton1408@gmail.com>
2026-01-27 20:38:21 -0500

    correcred sliding_window.py

--- sliding_window.py
+++ sliding_window.py
@@ -6,12 +6,14 @@ class Solution:
         left = 0
         
         for right in range(n):
-            left = self.new_method(s, charIndex, left, right)
+            if charIndex[ord(s[right])] >= left:
+                left = charIndex[ord(s[right])] + 1
+            charIndex[ord(s[right])] = right
             maxLength = max(maxLength, right - left + 1)
         
         return maxLength
         
-        
+
 solution = Solution()
 print(solution.lengthOfLongestSubstring("abcabcbb"))  # Output: 3
 print(solution.lengthOfLongestSubstring("bbbbb"))     # Output: 1
