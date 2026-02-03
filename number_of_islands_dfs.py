6be5895ce7626e034ff59e06bc2235823b26c22f
Anton <ivanko.anton1408@gmail.com>
2026-02-03 18:30:09 -0500

    number_of_islands dfs problem

---
+++ number_of_islands_dfs.py
@@ -0,0 +1,44 @@
+from typing import List
+from collections import deque
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        islands = 0
+        visited = set()
+        rows, cols = len(grid), len(grid[0])
+
+        def bfs(r, c):
+            q = deque()
+            visited.add((r, c))
+            q.append((r, c))
+
+            while q:
+                row, col = q.popleft()
+                directions = [[1,0],[-1,0],[0,1],[0,-1]]
+
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if 0 <= r < rows and 0 <= c < cols and grid[r][c] == "1" and (r, c) not in visited:
+                        q.append((r, c))
+                        visited.add((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visited:
+                    islands += 1
+                    bfs(r, c)
+
+        return islands
+    
+    
+# Example usage:
+solution = Solution()
+grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
+  ["1","1","0","0","0"],
+  ["0","0","0","0","1"]
+]
+result = solution.numIslands(grid)
+print(result)  # Expected output: 2 
