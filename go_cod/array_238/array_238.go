741024918f6e4f3f7f497e6c22414faa988ed1ce
Anton <ivanko.anton1408@gmail.com>
2026-03-10 17:05:44 -0400

    array problem 238

---
+++ go_cod/array_238/array_238.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func productExceptSelf(nums []int) []int {
+	n := len(nums)
+	right := make([]int, n)
+	res := make([]int, n)
+
+	prod := 1
+	for i := n - 1; i >= 0; i-- {
+		prod *= nums[i]
+		right[i] = prod
+	}
+
+	prod = 1
+	for i := 0; i < n-1; i++ {
+		lp := prod
+		rp := right[i+1]
+
+		res[i] = rp * lp
+		prod *= nums[i]
+	}
+	res[n-1] = prod
+	return res
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4}
+	result := productExceptSelf(nums)
+	fmt.Println(result)
+}
