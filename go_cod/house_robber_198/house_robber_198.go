dc956133b9a190a225e214c553827015904db249
Anton <ivanko.anton1408@gmail.com>
2026-03-31 19:16:07 -0400

    house robber problem

---
+++ go_cod/house_robber_198/house_robber_198.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func rob(nums []int) int {
+	dp := make([]int, len(nums))
+	if len(nums) == 1 {
+		return nums[0]
+	}
+
+	dp[0] = nums[0]
+	dp[1] = maxInt(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		dp[i] = maxInt(dp[i-2]+nums[i], dp[i-1])
+	}
+
+	return dp[len(nums)-1]
+}
+
+func maxInt(a int, b int) int {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	nums := []int{2, 1, 1, 2}
+	fmt.Println(rob(nums)) // Output: 4
+}
